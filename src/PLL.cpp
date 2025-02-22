d1867982cb933064eb4b63f16e4b6817ddb0feed
unknown <rhasa@illinois.edu>
2025-02-22 17:42:58 -0600

    PLL taeaks

--- src/PLL.cpp
+++ src/PLL.cpp
@@ -6,53 +6,32 @@
 class PLL{
  
 private:
-
-    //float sim_dt = 1/20000;
-
-    float bandwidth;
-    float last_update_time;
     float pll_kp;
     float pll_ki;
-    float last_measurement;
-    float pll_pos;
-    float pll_vel;
-    float pll_acc;
-    
-    Encoder encoder;
+    float pll_pos = 0;
+    float pll_vel = 0;
 
-public:
 
+    public:
     PLL(float pll_bandwidth){
-
-        bandwidth = pll_bandwidth;
-        last_update_time = 0;
-        pll_kp = 2*bandwidth;
+        pll_kp = 2.0*pll_bandwidth;
         pll_ki = 0.25*pll_kp*pll_kp;
-
-        pll_pos = 0;
-        pll_vel = 0;
-        pll_acc = 0;
-
-
     }
 
 
-    void update(float dt){
-
+    void update(float dt, Encoder encoder){
         int32_t measurement = encoder.read();
-        pll_pos = pll_pos + (dt*pll_vel);
+        pll_pos += dt * pll_vel;
 
         float delta_pos = measurement - floor(pll_pos);
 
-        pll_pos = pll_pos + (dt * pll_kp * delta_pos);
-        pll_vel = pll_vel + (dt * pll_ki * delta_pos);
+        pll_pos += dt * pll_kp * delta_pos;
+        pll_vel += dt * pll_ki * delta_pos;
 
         float margin = 0.5*dt*pll_ki;
         if(fabs(pll_vel <= margin)){
             pll_vel = 0;
         }
-
-        last_measurement = measurement;
     }
 
 

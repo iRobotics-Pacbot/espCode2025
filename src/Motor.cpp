55a12f31f85c76470aff391f27c93b4a2865623c
Tyler Andreas <tylerba2@illinois.edu>
2025-02-26 18:21:33 -0600

    fixed min and max in Motor

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -2,7 +2,7 @@
 #include <Encoder.h>
 
 #include <Motor.h>
-
+#include <algorithm>
 // some rough testing suggests min 5k based on electrical time constant
 #define FREQ 10000
 #define RES 15
@@ -11,12 +11,12 @@ Motor::Motor(uint8_t ph, uint8_t en): ph(ph), en(en) {
     pinMode(en, OUTPUT);
     pinMode(ph, OUTPUT);
     
-    analogWriteFrequency(en, FREQ);
+    analogWriteFrequency(FREQ);
     analogWriteResolution(RES);
 }
 
 void Motor::setThrottle(double throttle) {
-    throttle = min(max(throttle, -1), 1);
+    throttle = std::min(std::max(throttle,(double) -1), (double)1);
     int16_t pwm_duty = abs(throttle) * 0x7fffp0;
     if (throttle > 0) {
         digitalWrite(ph, HIGH);
